36afa2a969c05a6fd62aec3a80aaaa4c6b22ca00
mignonstyle <mignonxstyle@gmail.com>
2017-06-29 18:44:27 +0900

    modify travis

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,7 +4,7 @@
  *
  * @package ms-custom-login
  */
-
+/*
 $_tests_dir = getenv('WP_TESTS_DIR');
 if ( !$_tests_dir ) {
 	$_tests_dir = '/tmp/wordpress-tests-lib';
@@ -12,10 +12,10 @@ if ( !$_tests_dir ) {
 
 // Give access to tests_add_filter() function.
 require_once $_tests_dir . '/includes/functions.php';
-
+*/
 /**
  * Manually load the plugin being tested.
- */
+ *//*
 function _manually_load_plugin() {
 	require dirname( __FILE__ ) . '/ms-custom-login.php';
 }
@@ -23,3 +23,4 @@ tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 // Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';
+*/
